3301e90da421aaf266bc4d99899b397833b7fee9
eamoses <e.a.moses@gmail.com>
2022-10-17 21:02:57 -0500

    save

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -6,16 +6,16 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
-
+var dotenv = require('dotenv').config()
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = `${dotenv.parsed.CLIENT_ID}`; // Your client id
+var client_secret = `${dotenv.parsed.CLIENT_SECRET}`; // Your secret
+var redirect_uri = `${dotenv.parsed.REDIRECT_URI}`; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
